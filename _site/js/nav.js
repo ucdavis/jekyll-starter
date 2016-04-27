daf58a7ac62c3053f7b6562e9f56c7a96804070a
Cal <cydoval@mac.com>
2016-04-27 14:56:59 -0700

    Revert "were back to failure land"
    This reverts commit 6fc0658c5025371ddd81ea845dbcdff05047f169.

--- _site/js/nav.js
+++ _site/js/nav.js
@@ -1,48 +1,45 @@
-jQuery(document).ready(function($){
-  //move nav element position according to window width
-  moveNavigation();
-  $(window).on('resize', function(){
-    (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
-  });
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
 
-  //mobile version - open/close navigation
-  $('.cd-nav-trigger').on('click', function(event){
-    event.preventDefault();
-    if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
-    
-    $('header').toggleClass('nav-is-visible');
-    $('.cd-main-nav').toggleClass('nav-is-visible');
-    $('.cd-main-content').toggleClass('nav-is-visible');
-  });
 
-  //mobile version - go back to main navigation
-  $('.go-back').on('click', function(event){
-    event.preventDefault();
-    $('.cd-main-nav').removeClass('moves-out');
-  });
 
-  //open sub-navigation
-  $('.cd-subnav-trigger').on('click', function(event){
-    event.preventDefault();
-    $('.cd-main-nav').toggleClass('moves-out');
-  });
+;(function( $, window, document, undefined )
+{
+	$.fn.doubleTapToGo = function( params )
+	{
+		if( !( 'ontouchstart' in window ) &&
+			!navigator.msMaxTouchPoints &&
+			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i ) ) return false;
 
-  function moveNavigation(){
-    var navigation = $('.cd-main-nav-wrapper');
-      var screenSize = checkWindowWidth();
-        if ( screenSize ) {
-          //desktop screen - insert navigation inside header element
-      navigation.detach();
-      navigation.insertBefore('.cd-nav-trigger');
-    } else {
-      //mobile screen - insert navigation after .cd-main-content element
-      navigation.detach();
-      navigation.insertAfter('.cd-main-content');
-    }
-  }
+		this.each( function()
+		{
+			var curItem = false;
 
-  function checkWindowWidth() {
-    var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
-    return ( mq == 'mobile' ) ? false : true;
-  }
-});
+			$( this ).on( 'click', function( e )
+			{
+				var item = $( this );
+				if( item[ 0 ] != curItem[ 0 ] )
+				{
+					e.preventDefault();
+					curItem = item;
+				}
+			});
+
+			$( document ).on( 'click touchstart MSPointerDown', function( e )
+			{
+				var resetItem = true,
+					parents	  = $( e.target ).parents();
+
+				for( var i = 0; i < parents.length; i++ )
+					if( parents[ i ] == curItem[ 0 ] )
+						resetItem = false;
+
+				if( resetItem )
+					curItem = false;
+			});
+		});
+		return this;
+	};
+})( jQuery, window, document );
